e794acfbf8f68e7ab468494439d22a0762e23fd0
John Kim <jkim@rowlandschools.org>
2018-10-11 21:53:41 -0700

    Add files via upload

---
+++ gravity.py
@@ -0,0 +1,17 @@
+def calcFallTime(t, v, h):
+    h=-16*t*t+v*t+h
+    return h
+
+
+print "Calculate the height you will achieve"
+
+time=0.6 
+velocity=16
+height=16
+
+final=calcFallTime(time, velocity, height)
+print "After 1.6 seconds with a starting"
+print "velocity of 16 ft/sec and a starting"
+print "height of 16 feet"
+print ""
+print "You will achieve "+str(final)+" feet"
